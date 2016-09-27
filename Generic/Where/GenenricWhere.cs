73fda88b52fe1c751cb8d5b3ef3cc468abb15608
deepshp <preetdkit@yahoo.dk>
2016-09-27 12:15:31 +0200

    Where Genric

---
+++ Generic/Where/GenenricWhere.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic.Where
+{
+//This class can only accept value type like int ,double, bit ,even included  Struc
+    class ValueTypeData<T> where T :struct
+    {
+
+
+    }
+
+    //This class can only accept which class use comparable 
+    class CompareaBale<T> where T : IComparable
+    {
+
+    }
+
+    class  ReferenceType<T> where T: class,new()
+    {
+
+    }
+
+
+
+}
