2404cd30d01f9a5ea12ca6c09169f4bdcdb96891
deepshp <preetdkit@yahoo.dk>
2016-10-04 14:36:14 +0200

    Abtract

---
+++ AbstractClassAndInterface/BaseAbstarct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAndInterface
+{
+    abstract class  BaseAbstarct
+    {
+        public BaseAbstarct()
+        {
+
+        }
+        public abstract void abstractMethod();
+        public void GetAbstractClass()
+        {
+
+        }
+    }
+     class ChildClass :BaseAbstarct
+    {
+        public override void abstractMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+       
+
+        public void GetAbstractClass()
+        {
+
+        }
+    }
+}
